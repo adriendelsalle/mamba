cea135ef61f1d043977b3a220195c6aeb9e1bf78
Adrien DELSALLE <ad.delsalle@gmail.com>
2021-07-13 09:38:28 +0200

    improve completer for env names add bash function for completion

--- src/micromamba/completer.cpp
+++ src/micromamba/completer.cpp
@@ -15,38 +15,43 @@ string_comparison(const std::string& a, const std::string& b)
 }
 
 void
-complete_options(CLI::App* app, const std::string& last_arg, bool& completed)
+complete_options(CLI::App* app, const std::vector<std::string>& last_args, bool& completed)
 {
-    if (completed)
+    if (completed || last_args.empty())
         return;
     completed = true;
 
-    if (last_arg == "-n")
+    if (last_args[0] == "-n" && last_args.size() == 2)
     {
         auto& config = mamba::Configuration::instance();
         config.at("show_banner").set_value(false);
         config.load();
 
         auto root_prefix = config.at("root_prefix").value<fs::path>();
+        auto& name_start = last_args.back();
 
         std::vector<std::string> prefixes;
         if (fs::exists(root_prefix / "envs"))
             for (const auto& p : fs::directory_iterator(root_prefix / "envs"))
                 if (p.is_directory() && fs::exists(p / "conda-meta"))
-                    prefixes.push_back(p.path().filename().string());
+                {
+                    auto name = p.path().filename().string();
+                    if (mamba::starts_with(name, name_start))
+                        prefixes.push_back(name);
+                }
 
         std::sort(prefixes.begin(), prefixes.end(), string_comparison);
         std::cout << mamba::printers::table_like(prefixes, 90).str() << std::endl;
     }
-    else if (mamba::starts_with(last_arg, "-"))
+    else if (mamba::starts_with(last_args.back(), "-"))
     {
-        auto opt_start = mamba::lstrip(last_arg, "-");
+        auto opt_start = mamba::lstrip(last_args.back(), "-");
 
         if (opt_start.empty())
             return;
 
         std::vector<std::string> options;
-        if (mamba::starts_with(last_arg, "--"))
+        if (mamba::starts_with(last_args.back(), "--"))
             for (const auto* opt : app->get_options())
             {
                 for (const auto& n : opt->get_lnames())
@@ -69,12 +74,23 @@ complete_options(CLI::App* app, const std::string& last_arg, bool& completed)
 int
 get_completions(CLI::App* app, int argc, char** argv)
 {
+    std::vector<std::string> completer_args;
     bool completed = false;
+
     CLI::App* install_subcom = app->get_subcommand("install");
 
-    app->callback([&]() { complete_options(app, argv[argc - 1], completed); });
+    if (argc > 2 && std::string(argv[argc - 2]) == "-n")
+    {
+        completer_args.push_back(argv[argc - 2]);
+        completer_args.push_back(argv[argc - 1]);
+        argc -= 1;
+    }
+    else
+        completer_args.push_back(argv[argc - 1]);
+
+    app->callback([&]() { complete_options(app, completer_args, completed); });
     install_subcom->callback(
-        [&]() { complete_options(install_subcom, argv[argc - 1], completed); });
+        [&]() { complete_options(install_subcom, completer_args, completed); });
 
     argv[1] = argv[0];
     CLI11_PARSE(*app, argc - 2, argv + 1);
